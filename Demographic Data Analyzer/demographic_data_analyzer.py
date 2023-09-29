68a04c7c77b65e6ed5685c3140c8e38add23feb2
Kieran Thakkar <kieranthakkar@gmail.com>
2023-09-30 00:22:03 +0100

    rounded Q1 and Q2 to 1dp to avoid d.p. error

--- Demographic Data Analyzer/demographic_data_analyzer.py
+++ Demographic Data Analyzer/demographic_data_analyzer.py
@@ -9,10 +9,10 @@ def calculate_demographic_data(print_data=True):
     race_count = df["race"].value_counts()
 
     # What is the average age of men?
-    average_age_men = df[df["sex"] == "Male"]["age"].mean()
+    average_age_men = round(df[df["sex"] == "Male"]["age"].mean(),1)
 
     # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = 100*df[df["education"] == "Bachelors"].shape[0]/df.shape[0]
+    percentage_bachelors = round(100*df[df["education"] == "Bachelors"].shape[0]/df.shape[0],1)
 
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
     # What percentage of people without advanced education make more than 50K?
