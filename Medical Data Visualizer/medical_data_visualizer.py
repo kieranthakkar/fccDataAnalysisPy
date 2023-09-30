9c4893808311b45f4828017f1af3d1baaee3388e
Kieran Thakkar <kieranthakkar@gmail.com>
2023-09-30 17:29:09 +0100

    Parts 1 and 2 done

--- Medical Data Visualizer/medical_data_visualizer.py
+++ Medical Data Visualizer/medical_data_visualizer.py
@@ -4,14 +4,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Import data
-df = None
+df = pd.read_csv("Medical Data Visualizer\medical_examination.csv")
 
 # Add 'overweight' column
-df['overweight'] = None
+#To determine if a person is overweight, first calculate their BMI by dividing their weight in kilograms by the square of their height in meters. If that value is > 25 then the person is overweight. Use the value 0 for NOT overweight and the value 1 for overweight.
+df['overweight'] = df["weight"]/((df["height"]/100)**2)
+df['overweight'] = df['overweight'].apply(lambda x : 1 if x > 25 else 0)
 
 # Normalize data by making 0 always good and 1 always bad. If the value of 'cholesterol' or 'gluc' is 1, make the value 0. If the value is more than 1, make the value 1.
+df["cholesterol"] = df["cholesterol"].apply(lambda x : 1 if x > 1 else 0)
+df["gluc"] = df["gluc"].apply(lambda x : 1 if x > 1 else 0)
 
-
+print(df)
 # Draw Categorical Plot
 def draw_cat_plot():
     # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
