203a5fb0a9466ed7a26dcc34df5d60121ebb9dc0
Kieran Thakkar <k.thakkar@hotmail.co.uk>
2023-10-14 11:27:36 +0100

    cleaning conditions for heatmap

--- Medical Data Visualizer/medical_data_visualizer.py
+++ Medical Data Visualizer/medical_data_visualizer.py
@@ -34,7 +34,12 @@ def draw_cat_plot():
 # Draw Heat Map
 def draw_heat_map():
     # Clean the data
-    df_heat = None
+    df_heat = df.loc[
+    (df['height'] >= df['height'].quantile(0.025)) &
+    (df['height'] <= df['height'].quantile(0.975)) &
+    (df['weight'] >= df['weight'].quantile(0.025)) &
+    (df['weight'] <= df['weight'].quantile(0.975)) &
+    (df['ap_lo'] <= df['ap_hi'])] 
 
     # Calculate the correlation matrix
     corr = None
